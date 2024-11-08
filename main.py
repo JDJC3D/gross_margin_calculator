6be69dddddc4f716e3e461c0a6e3828ecdabd1a6
James <38363488+JDJC3D@users.noreply.github.com>
2024-11-08 08:58:27 +0000

    TODO List added

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def calculate_gm():
 
     cost_price = float(price_entry.get())
     margin = float(markup_entry.get())
-
     selling_price = round(cost_price / (1 - ( margin / 100) ), 2)
 
     output_label.configure(app, text=f"£{selling_price}")
@@ -36,3 +35,12 @@ formula_label = CTkLabel(app, text="Formula: COST_PRICE / ( 1 - MARGIN / 100 )")
 formula_label.pack(padx=20, pady=10)
 
 app.mainloop()
+
+# TODO 1: Redo UI with labels (Add footer with my details as well (name, copright, github))
+# TODO 2: Move from Pack to Grid
+# TODO 3: Add Error catching
+# TODO 4: Fix bug with number missing 0 when using 10.00 and 60% markup
+# TODO 5: Add a custom font
+# TODO 6: Create a custom Icon (Get Canva for this)
+# TODO 7: Create exe for desktop use (see notes on customtkinter site)
+# TODO 8: Add Exit button
